cae71ad68a21797a24eb67ee8a9f429acd3b9463
alexanderalban <albanalexander@gmail.com>
2020-10-05 11:16:48 -0500

    Created Function

---
+++ myscripts.js
@@ -0,0 +1,23 @@
+
+//time to give fizzbuzz a go
+
+function FizzAndBuzz() {
+    console.log("Fizz")
+    for ( i=0; i < 1001; i++){
+        
+        if (i % 15 == 0) {
+        console.log("FizzBuzz");
+
+        }else if (i % 3 == 0) {
+        console.log("Fizz");
+
+        }else if (i % 5 == 0) {
+        console.log("Buzz");
+
+        }else console.log(i); {
+        }
+
+
+    }
+
+}
